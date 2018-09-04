70e6f24a19e100b029508ad9700ec1906d22ebfd
parisa pahlevan <paris.pahlevan@gmail.com>
2018-09-04 17:45:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+const main = document.querySelector("#main")
+main.addEventListener("click", (event) =>{
+  alert("Hey you clicked me")
+})
+const paragraph = document.querySelector("p") 
+paragraph.addEventListener("click", (event) =>{
+  alert("You clicked the p!!")
+  paragraph.style.backgroundColor = "yellow"
+})
+const shoppingPage = document.querySelector
+function  bubble(event){
+  console.log(this.firstChild.nodeValue.trim() + "bubbled")
+  this.style.backgroundColor
+}
+
